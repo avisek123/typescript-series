2ff1432064e696aca7e3db4ceb5abe9376f78958
avisek123 <mrperfectavi89@gmail.com>
2021-11-28 18:14:35 +0530

    any and enum use

---
+++ enum.js
@@ -0,0 +1,7 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["MANGER"] = 1] = "MANGER";
+    Role[Role["READ_ONLY_USER"] = 2] = "READ_ONLY_USER";
+})(Role || (Role = {}));
+console.log(Role.ADMIN);
