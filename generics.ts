70ceef069cb6153841cfa21d16c497258faeefc2
avisek123 <mrperfectavi89@gmail.com>
2022-05-10 21:06:27 +0530

    generics in interface

--- generics.ts
+++ generics.ts
@@ -6,3 +6,16 @@ const addUid = <T extends object>(obj: T) => {
 /* call the function  */
 const obj = addUid({ name: 'John', age: 12, height: 12 });
 console.log(obj.name)
+
+
+/* with the interface  */
+interface Resource<T> {
+    uid: number,
+    name: string,
+    data: T
+}
+const result: Resource<string> = {
+    uid: 1,
+    name: 'John',
+    data: "some data"
+}
