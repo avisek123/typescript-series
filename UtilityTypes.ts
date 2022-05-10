b316d8c69afd9f30f106c856b0a222066c1a7a81
avisek123 <mrperfectavi89@gmail.com>
2022-05-10 21:36:42 +0530

    Required types done

--- UtilityTypes.ts
+++ UtilityTypes.ts
@@ -3,6 +3,13 @@ interface MyUser {
     age: number;
     address: string;
 }
+/* ========================= Partial Types ================================ */
 /* Here we are going to use partial types , in that we have to declare new type  */
 type PartialUser = Partial<MyUser>;
 /* in partialUser type all attributes are optional */
+
+/* ========================= Required Types ================================ */
+/* Here we are going to use partial types , in that we have to declare new type  */
+type RequiredUser = Required<MyUser>;
+/* in partialUser type all attributes are required */
+
