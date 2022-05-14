69c06764f767b1ccf79836cc4a8b1777ba343a03
avisek123 <mrperfectavi89@gmail.com>
2022-05-14 11:11:00 +0530

    KeyOf with generics

--- keyOfTypes.ts
+++ keyOfTypes.ts
@@ -4,12 +4,21 @@ type user = {
     id: string
 
 }
+type account = {
+    name: string;
+    acNumber: string;
+}
 let measure: user = {
     name: '',
     age: '',
     id: ''
 
 }
+let myAccount: account = {
+    name: '',
+    acNumber: ''
+}
+
 /* function without keyof types */
 const functionChangeKeyValue = (object: user, key: 'name' | 'id', value: string) => {
     object[key] = value;
@@ -19,13 +28,14 @@ const functionChangeKeyValue = (object: user, key: 'name' | 'id', value: string)
 functionChangeKeyValue(measure, 'name', 'ramesh');
 
 /* function with keyof types */
-const functionChangeKeyValue1 = (object: user, key: keyof user, value: user[keyof user]) => {
+const functionChangeKeyValue1 = <T>(object: T, key: keyof T, value: T[keyof T]) => {
     object[key] = value;
     return object;
 
 
 }
 functionChangeKeyValue1(measure, 'name', '');
+functionChangeKeyValue1(myAccount, 'name', '');
 
 
 
